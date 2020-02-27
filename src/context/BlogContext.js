82c80157fb77866559214344874d596a3b10e377
Nemanja Stojanovic <s.nemanja@protonmail.ch>
2020-02-27 01:32:11 +0100

    Init simple blog app

---
+++ src/context/BlogContext.js
@@ -0,0 +1,45 @@
+import createDataContext from './createDataContext';
+
+const blogReducer = (state, action) => {
+    switch(action.type) {
+        case 'add_blogpost':
+            return [
+                ...state,
+                {
+                    id: Math.floor(Math.random() * 99999),
+                    title: action.payload.title,
+                    content: action.payload.content
+                }
+            ];
+        case 'delete_blogpost':
+            return state.filter(blogPost => blogPost.id !== action.payload);
+        case 'edit_blogpost':
+            return state.map(blogPost => blogPost.id === action.payload.id ? action.payload : blogPost);
+        default:
+            return state;
+    }
+};
+
+const addBlogPost = dispatch => (
+    (title, content, callback) => {
+        dispatch({ type: 'add_blogpost', payload: { title, content } })
+        if(callback) callback();
+    }
+);
+
+const deleteBlogPost = dispatch => (
+    id => dispatch({ type: 'delete_blogpost', payload: id })
+);
+
+const editBlogPost = dispatch => (
+    (id, title, content, callback) => {
+        dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
+        if(callback) callback();
+    }
+);
+
+export const { Context, Provider } = createDataContext(
+    blogReducer,
+    { addBlogPost, deleteBlogPost, editBlogPost },
+    [{ id: 1, title: 'TEST POST TITLE', content: 'TEST POST CONTENT' }]
+);
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,54 @@
+import { View, Text, FlatList, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import React, { useContext } from 'react';
+import { Context } from "../context/BlogContext";
+
+const IndexScreen = ({ navigation }) => {
+    const { state, deleteBlogPost } = useContext(Context);
+
+    return (
+        <View>
+            <FlatList
+                data={state}
+                keyExtractor={item => item.title}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Feather style={styles.icon} name='trash' />
+                            </TouchableOpacity>
+                        </View>
+                    </TouchableOpacity>
+                )}
+            />
+        </View>
+    )
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => ({
+    headerRight: (
+        <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+            <Feather name='plus' size={30} />
+        </TouchableOpacity>
+    )
+});
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'grey'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+});
+
+export default IndexScreen;
